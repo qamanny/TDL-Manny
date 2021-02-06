71a76de718c5ca740df8fa879fcfa6582f796409
Emmanuel <qamanny@academytrainee.com>
2021-02-06 14:16:29 +0000

    Added Domain object

---
+++ src/main/java/com/qa/persistence/domain/Task.java
@@ -0,0 +1,74 @@
+package com.qa.persistence.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String taskName;
+	
+	@Column
+	private Date deadline;
+	
+	@Column
+	private Boolean completed;
+
+	public Task() {
+		super();
+	}
+
+	public Task(Long id, String taskName, Date deadline, Boolean completed) {
+		super();
+		this.id = id;
+		this.taskName = taskName;
+		this.deadline = deadline;
+		this.completed = completed;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public Date getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+
+	public Boolean getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
+	}
+	
+	
+	
+
+}
