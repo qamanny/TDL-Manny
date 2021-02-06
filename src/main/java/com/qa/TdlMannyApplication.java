a72e17a87ed62797c1b509e0cf40a455ad418012
Emmanuel <qamanny@academytrainee.com>
2021-02-06 14:06:18 +0000

    A bit of configuration

--- src/main/java/com/qa/TdlMannyApplication.java
+++ src/main/java/com/qa/TdlMannyApplication.java
@@ -2,12 +2,14 @@ package com.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TdlMannyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TdlMannyApplication.class, args);
+		ApplicationContext beanBag = SpringApplication.run(TdlMannyApplication.class, args);
+		System.out.println(beanBag.getBean("HelloWorld", String.class));
 	}
 
 }
