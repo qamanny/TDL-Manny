d9861252243648a7b351e919d34851e58d57c415
Emmanuel <qamanny@academytrainee.com>
2021-02-06 13:58:16 +0000

    Starter Files

---
+++ src/main/java/com/qa/TdlMannyApplication.java
@@ -0,0 +1,13 @@
+package com.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlMannyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdlMannyApplication.class, args);
+	}
+
+}
