6bb4456b30c178a120594daad7e73485b0533e1d
Emmanuel <qamanny@academytrainee.com>
2021-02-07 13:51:37 +0000

    Added TaskService

--- src/main/java/com/qa/rest/TaskController.java
+++ src/main/java/com/qa/rest/TaskController.java
@@ -3,9 +3,18 @@ package com.qa.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.rest.services.TaskService;
+
 @RestController
 @RequestMapping("/task")
 public class TaskController {
+	
+	private TaskService service;
+
+	public TaskController(TaskService service) {
+		super();
+		this.service = service;
+	}
 	
 	//Get
 	
---
+++ src/main/java/com/qa/rest/services/TaskService.java
@@ -0,0 +1,8 @@
+package com.qa.rest.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+}
