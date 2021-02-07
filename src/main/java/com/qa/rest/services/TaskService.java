035394d35b38e003da79007862468e233bfcd8bb
Emmanuel <qamanny@academytrainee.com>
2021-02-07 16:11:16 +0000

    Added ModelMapper to pom

--- src/main/java/com/qa/rest/services/TaskService.java
+++ src/main/java/com/qa/rest/services/TaskService.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import com.qa.dto.TaskDTO;
 import com.qa.persistence.domain.Task;
 import com.qa.persistence.domain.repos.TaskRepo;
 
@@ -13,6 +13,7 @@ import com.qa.persistence.domain.repos.TaskRepo;
 public class TaskService {
 	
 	private TaskRepo repo;
+	private ModelMapper mapper;
 
 	@Autowired
 	public TaskService(TaskRepo repo) {
@@ -27,7 +28,7 @@ public class TaskService {
 		//Post
 		
 		@PostMapping("/create")
-		public Task create(Task task) {
+		public TaskDTO create(Task task) {
 			return this.repo.save(task);
 		}
 		
