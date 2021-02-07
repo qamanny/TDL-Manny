c365fb07f8b0947f16c60e8c5e4a33ee72f731f6
Emmanuel <qamanny@academytrainee.com>
2021-02-07 16:01:16 +0000

    Working on create method

--- src/main/java/com/qa/rest/TaskController.java
+++ src/main/java/com/qa/rest/TaskController.java
@@ -1,8 +1,13 @@
 package com.qa.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.persistence.domain.Task;
 import com.qa.rest.services.TaskService;
 
 @RestController
@@ -11,6 +16,7 @@ public class TaskController {
 	
 	private TaskService service;
 
+	@Autowired
 	public TaskController(TaskService service) {
 		super();
 		this.service = service;
@@ -22,7 +28,11 @@ public class TaskController {
 	
 	//Post
 	
-	@PostMapping
+	@PostMapping("/create")
+	public Task create(@RequestBody Task task) {
+		return this.service.create(task);
+	}
+	
 	
 	//Put
 	
--- src/main/java/com/qa/rest/services/TaskService.java
+++ src/main/java/com/qa/rest/services/TaskService.java
@@ -1,8 +1,46 @@
 package com.qa.rest.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.qa.persistence.domain.Task;
+import com.qa.persistence.domain.repos.TaskRepo;
 
 @Service
 public class TaskService {
+	
+	private TaskRepo repo;
+
+	@Autowired
+	public TaskService(TaskRepo repo) {
+		super();
+		this.repo = repo;
+	}
+	
+	//Get
+	
+		@GetMapping
+		
+		//Post
+		
+		@PostMapping("/create")
+		public Task create(Task task) {
+			return this.repo.save(task);
+		}
+		
+		
+		//Put
+		
+		@PutMapping
+		
+		//Delete
+		
+		@DeleteMapping
+
+	
+	
 
 }
