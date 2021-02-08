d195fc98ce828827f668a0724f4cfc263fb5f72a
Emmanuel <qamanny@academytrainee.com>
2021-02-08 07:56:17 +0000

    Project DTO class created

--- src/main/java/com/qa/dto/ProjectDTO.java
+++ src/main/java/com/qa/dto/ProjectDTO.java
@@ -1,5 +1,38 @@
 package com.qa.dto;
 
+
 public class ProjectDTO {
+	
+	private Long id;
+	
+	private String projectName;
+
+	public ProjectDTO() {
+		super();
+	}
+
+	public ProjectDTO(Long id, String projectName) {
+		super();
+		this.id = id;
+		this.projectName = projectName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	
+	
 
 }
