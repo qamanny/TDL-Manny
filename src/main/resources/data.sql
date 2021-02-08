82fc39030ad168d7d6709c36c6b1ec08b8963659
Emmanuel <qamanny@academytrainee.com>
2021-02-08 08:37:42 +0000

    Created data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO PROJECT(PROJECT_NAME)
+VALUES
+('Learning Javascript'),
+('Six Pack'),
+('Backflip'),
+('To Do List'),
+('Learning German');
+
+INSERT INTO TASK(TASK_NAME, DEADLINE, COMPLETED)
+VALUES
+('Certificate on freecodeCamp', '2021-10-20', false),
+('Do 100 in a row Situps', '2021-02-10', false),
+('Find a gymnastics place', '2021-04-13', false),
+('Finish BackEnd and BackEnd Testing', '2021-02-09', false),
+('Finish Current Unit on Rosetta Stone', '2021-10-20', false);
