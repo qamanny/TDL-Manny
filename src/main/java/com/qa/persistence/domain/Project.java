e023bbab3b99ccce193c1ac2a954ba716e754720
Emmanuel <qamanny@academytrainee.com>
2021-02-08 07:54:37 +0000

    Project Domain class created

--- src/main/java/com/qa/persistence/domain/Project.java
+++ src/main/java/com/qa/persistence/domain/Project.java
@@ -1,5 +1,45 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String projectName;
+
+	public Project() {
+		super();
+	}
+
+	public Project(Long id, String projectName) {
+		super();
+		this.id = id;
+		this.projectName = projectName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	
+	
 
 }
